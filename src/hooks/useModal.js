aa2238a31c9c5f080d031adb59cc14dbc7d6c2b7
aipudiohelp <aipudio.help@gmail.com>
2026-09-11 13:42:12 +0300

    Update useModal.js

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,1 +1,14 @@
+"use client";
+import { useCart } from "./useCart";
 
+export function useModal() {
+  const { activeModal, selectedProduct, openOrderModal, openQuizModal, closeModal } = useCart();
+  return {
+    isOpen: activeModal !== null,
+    modalType: activeModal,
+    selectedProduct,
+    openOrderModal,
+    openQuizModal,
+    closeModal,
+  };
+}

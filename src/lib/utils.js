5a9b6e9c9df27e33b97aa8e20bddea76212484c2
aipudiohelp <aipudio.help@gmail.com>
2026-09-11 13:40:38 +0300

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,1 +1,31 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(price) {
+  return new Intl.NumberFormat("ar-EG", {
+    style: "currency",
+    currency: "EGP",
+    maximumFractionDigits: 0,
+  }).format(price);
+}
+
+export function generateWhatsAppLink({
+  phoneNumber = "201000000000",
+  productName,
+  price,
+  customerName = "",
+  customerAddress = "",
+}) {
+  let message = `مرحباً، أود إتمام طلب المنتج التالي:\n`;
+  message += `📦 *المنتج:* ${productName}\n`;
+  if (price) message += `💰 *السعر:* ${price} ج.م\n`;
+  if (customerName) message += `👤 *الاسم:* ${customerName}\n`;
+  if (customerAddress) message += `📍 *العنوان:* ${customerAddress}\n`;
+  message += `\nيرجى تأكيد موعد الشحن والاستلام والدفع عند الاستلام. شكراً لكم!`;
+
+  return `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+}

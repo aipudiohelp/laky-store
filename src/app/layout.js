4d42be70577ad2c2c5bdc139cd9be2695c0d692e
aipudiohelp <aipudio.help@gmail.com>
2026-09-16 10:36:04 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { CartProvider } from "@/hooks/useCart";
+import Script from "next/script";
 
 export const metadata = {
   title: "لكي ولأسرتك | عناية أفضل .. لحياة أجمل",
@@ -10,6 +11,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
+      <head>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "yj3kzpb616");
+          `}
+        </Script>
+      </head>
       <body className="antialiased selection:bg-brand-green selection:text-white">
         <CartProvider>
           {children}
